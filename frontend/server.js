5d78d12ed926b50a1005107ffca05aec537782f6
1001pepi <tanankemr@gmail.com>
2021-11-25 01:16:51 +0100

    13

---
+++ frontend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(__dirname + '/dist/frontend-eaa'));
+
+
+
+app.get('/*', function(req, res){
+    res.sendFile(path.join(__dirname + '/dist/frontend-eaa/index.html'));
+});
+
+app.listen(process.env.PORT || 8000);
